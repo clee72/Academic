03f9943998ab2d10bafdd12647f33ba2d4d3596f
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:38:25 -0400

    Update Good.cpp

--- OOP244/MILESTONES/MS3/Good.cpp
+++ OOP244/MILESTONES/MS3/Good.cpp
@@ -1,10 +1,10 @@
 // Good.cpp
-//Date: 2019/07/30
-//Section:SAB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//ms3
+// Date: 2019/07/30
+// Section:SAB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// ms3
 
 #pragma warning(disable:4996)
 #include <iostream>
