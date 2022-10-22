b700332adf6e82f2e360d5ebc3ae23439cc64f7e
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:37:17 -0400

    Update Error.h

--- OOP244/MILESTONES/MS2/Error.h
+++ OOP244/MILESTONES/MS2/Error.h
@@ -1,10 +1,10 @@
-//Error.h
-//Date: 2019/07/18
-//Section:SBB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//ms2
+// Error.h
+// Date: 2019/07/18
+// Section:SBB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// ms2
 
 #ifndef AID_ERROR_H
 #define AID_ERROR_H
