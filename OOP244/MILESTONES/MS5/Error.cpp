9446cdaf497bd1d89204907c4bc5d050ebab4369
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:42:40 -0400

    Update Error.cpp

--- OOP244/MILESTONES/MS5/Error.cpp
+++ OOP244/MILESTONES/MS5/Error.cpp
@@ -1,10 +1,10 @@
-//Error.cpp
-//Date: 2019/08/04
-//Section:SAB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//ms5
+// Error.cpp
+// Date: 2019/08/04
+// Section:SAB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// ms5
 
 #pragma warning(disable : 4996)
 #include<iostream>
