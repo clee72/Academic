8d0726e8307239ef8939593b68ae552d053355cf
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:30:58 -0400

    Update CustomerOrder.h

--- OOP345/MILESTONES/CustomerOrder.h
+++ OOP345/MILESTONES/CustomerOrder.h
@@ -1,11 +1,11 @@
-//Final Project: MS 3
+// Final Project: MS 3
 // Name: Wonhwa Lee	
 // Seneca Student ID: 076086149
 // Seneca email: wlee72@myseneca.ca
 // Date of completion: November 29, 2019
 //
 // I confirm that I am the only author of this file
-//   and the content was created entirely by me.
+// and the content was created entirely by me.
 
 #pragma once
 #include "Utilities.h"
