b1b04553edad801c91bbb984ffdb735959eddcd8
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:10:21 -0400

    Update String.h

--- OOP345/WORKSHOPS/WS1/String.h
+++ OOP345/WORKSHOPS/WS1/String.h
@@ -1,14 +1,14 @@
 
 // Workshop 1
-//Date: 2019/09/14
-//Section:NBB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//at_home
+// Date: 2019/09/14
+// Section:NBB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// at_home
 
 // I confirm that the content of this file is created by me,
-//   with the exception of the parts provided to me by my professor.
+// with the exception of the parts provided to me by my professor.
 
 
 #pragma once
@@ -18,7 +18,7 @@ namespace sdds {
 	extern unsigned g_maxSize;
 
 	class String {
-		char* m_str;//using pointer member variable to allocate memory dynamically
+		char* m_str;// using pointer member variable to allocate memory dynamically
 	public:
 		String(const char* str);
 		String& operator=(const String& str);
