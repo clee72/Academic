316b19c52df7406945d20432569782130aed6598
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:34:44 -0400

    Update CustomerOrder.h

--- OOP345/MILESTONES/CustomerOrder.h
+++ OOP345/MILESTONES/CustomerOrder.h
@@ -3,7 +3,7 @@
 // Seneca Student ID: 076086149
 // Seneca email: wlee72@myseneca.ca
 // Date of completion: November 29, 2019
-//
+
 // I confirm that I am the only author of this file
 // and the content was created entirely by me.
 
