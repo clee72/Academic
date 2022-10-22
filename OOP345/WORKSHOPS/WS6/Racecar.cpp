8fbd0950d12b77e4da051d34375e9128d450d0a4
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:27:19 -0400

    Update Racecar.cpp

--- OOP345/WORKSHOPS/WS6/Racecar.cpp
+++ OOP345/WORKSHOPS/WS6/Racecar.cpp
@@ -4,7 +4,7 @@
 // Date of completion:November 5, 2019
 //
 // I confirm that the content of this file is created by me,
-//   with the exception of the parts provided to me by my professor.
+// with the exception of the parts provided to me by my professor.
 
 #include "Racecar.h"
 #include <iostream>
