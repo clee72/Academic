d0606d382b07f0c853574077e952afaee9c4c7be
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:42:22 -0400

    Update Date.h

--- OOP244/MILESTONES/MS5/Date.h
+++ OOP244/MILESTONES/MS5/Date.h
@@ -1,10 +1,10 @@
-//Date.h
-//Date: 2019/08/04
-//Section:SAB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//ms5
+// Date.h
+// Date: 2019/08/04
+// Section:SAB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// ms5
 
 #pragma once
 #ifndef AID_DATE_H
