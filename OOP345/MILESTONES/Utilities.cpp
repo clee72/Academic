e766ac49bc5dd721efe3ee8aed711a2dd6df6d98
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:33:37 -0400

    Update Utilities.cpp

--- OOP345/MILESTONES/Utilities.cpp
+++ OOP345/MILESTONES/Utilities.cpp
@@ -1,11 +1,11 @@
-//Final Project: MS 3
+// Final Project: MS 3
 // Name: Wonhwa Lee	
 // Seneca Student ID: 076086149
 // Seneca email: wlee72@myseneca.ca
 // Date of completion: November 29, 2019
-//
+
 // I confirm that I am the only author of this file
-//   and the content was created entirely by me.
+// and the content was created entirely by me.
 
 #include "Utilities.h"
 #include <fstream>
@@ -22,7 +22,8 @@ size_t Utilities::getFieldWidth() const {
 
 const std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more) {
 	std::string t;
-	auto new_pos = str.find(getDelimiter(), next_pos);//find delimeter from the index # next_pos and return the postion of the delimeter
+	// find delimeter from the index # next_pos and return the postion of the delimeter
+	auto new_pos = str.find(getDelimiter(), next_pos);
 	t = str.substr(next_pos, new_pos - next_pos);
 	if (t != "") more = true;
 
