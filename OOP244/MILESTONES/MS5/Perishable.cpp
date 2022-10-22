353db46da7480dc17fe748d6a6b2f3f5a71f92e6
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:43:58 -0400

    Update Perishable.cpp

--- OOP244/MILESTONES/MS5/Perishable.cpp
+++ OOP244/MILESTONES/MS5/Perishable.cpp
@@ -1,10 +1,10 @@
-//Perishable.cpp
-//Date: 2019/08/04
-//Section:SAB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//ms5
+// Perishable.cpp
+// Date: 2019/08/04
+// Section:SAB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// ms5
 
 #include <iostream>
 #include<fstream>
