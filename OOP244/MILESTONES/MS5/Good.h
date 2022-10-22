bf7f329b76313a03add0592da7971872923a0cf3
clee72 <108632993+clee72@users.noreply.github.com>
2022-10-22 10:43:37 -0400

    Update Good.h

--- OOP244/MILESTONES/MS5/Good.h
+++ OOP244/MILESTONES/MS5/Good.h
@@ -1,10 +1,10 @@
 // Good.h
-//Date: 2019/08/04
-//Section:SAB
-//Name:WONHWA LEE
-//Student No.:076086149
-//Email:wlee72@myseneca.ca
-//ms5
+// Date: 2019/08/04
+// Section:SAB
+// Name:WONHWA LEE
+// Student No.:076086149
+// Email:wlee72@myseneca.ca
+// ms5
 
 #pragma once
 #ifndef AID_GOOD_H
